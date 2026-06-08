4f61a2f6b5a1a39415742b04e7e7171292434a6f
Luzu4 <maciekluz@gmail.com>
2026-06-08 22:58:47 +0200

    changes exercise-log to exercise-logs

--- src/api/exerciseLog.ts
+++ src/api/exerciseLog.ts
@@ -2,11 +2,11 @@ import type { ExerciseLog } from "../types";
 import client from "./client";
 
 export const logExerciseSet = async (exerciseLog: ExerciseLog): Promise<ExerciseLog> => {
-    const { data } = await client.post(`/exercise-log`, exerciseLog)
+    const { data } = await client.post(`/exercise-logs`, exerciseLog)
     return data
 }
 
 export const getPreviousExerciseLogsByWorkoutTemplateId = async (workoutTemplateId: number): Promise<ExerciseLog[]> => {
-    const { data } = await client.get(`/exercise-log/previous?workoutTemplateId=${workoutTemplateId}`)
+    const { data } = await client.get(`/exercise-logs/previous?workoutTemplateId=${workoutTemplateId}`)
     return data;
 }
